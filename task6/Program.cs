913602baa1b78c60194cb7058dcf0c999a1b29f0
Eldar Skhatum <79996321123@ya.ru>
2022-11-12 00:23:36 +0400

    added task 6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter the number ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.WriteLine("YES! IT'S EVEN!");
+}
+
+else
+{
+    Console.WriteLine("NO! IT'S ODD!");
+}
