9885abf5a8a7c1cea2b86bab4d9247451ce8028e
Eldar Skhatum <79996321123@ya.ru>
2022-11-12 00:34:39 +0400

    added task 8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter the number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int even = 2;
+
+while (N >= even)
+{
+    Console.Write($"{even} ");
+    even = even + 2;
+}
