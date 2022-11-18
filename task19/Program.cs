91ac80136cbc8029bc37232fba24a88eaf20f911
Eldar Skhatum <79996321123@ya.ru>
2022-11-18 23:05:16 +0400

    added homework and tasks 19,21,23

---
+++ task19/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Enter the 5 digit number");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+
+if (numberText.Length == 5 & numberText[0] == numberText[4] & numberText[1] == numberText[3])
+{
+    Console.WriteLine("YES! It's a palindrome!");
+}
+else
+{
+    Console.WriteLine("NOPE! It's not a palindrome!");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Enter X coordinate of point A");
+double xa = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Y coordinate of point A");
+double ya = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Z coordinate of point A");
+double za = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter X coordinate of point B");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Y coordinate of point B");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Enter Z coordinate of point B");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"The distance between A and B is {Math.Round(Math.Sqrt(Math.Pow(xa-xb, 2) + Math.Pow(ya-yb, 2) + Math.Pow(za-zb, 2)) , 2)}");
