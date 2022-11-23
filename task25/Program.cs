bcfad7777eed31c66c3574eb20393e0d0c4ae77b
Eldar Skhatum <79996321123@ya.ru>
2022-11-23 23:28:04 +0300

    added tasks 25, 27, 29

---
+++ task25/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// Console.WriteLine("Enter the number:");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Enter the degree:");
+// int B = Convert.ToInt32(Console.ReadLine());
+// int deg = 1;
+
+// for (int i = 1; i <= B; i++)
+// {
+//     deg = deg * A;
+// }
+
+// Console.WriteLine("The number " + A + " in degree " + B + " is " + deg);
+
+int Degree(int A, int B)
+{
+    int deg = 1;
+
+    for (int i = 1; i <= B; i++)
+    {
+    deg = deg * A;
+    }
+return deg;
+}
+
+Console.WriteLine("Enter the number:");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the degree:");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int res = Degree(A, B);
+Console.WriteLine(res);
---
+++ task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+if(number > 0)
+{
+    while (number > 0)
+{
+    int digit = number % 10;
+    number = number / 10;
+    sum = sum + digit;
+}
+Console.WriteLine("The total is " + sum);
+}
+
+else
+{
+    number = Math.Abs(number);
+    while (number > 9)
+{
+    int digit = number % 10;
+    number = number / 10;
+    sum = sum + digit;
+}
+Console.WriteLine($"The total is {sum - number}");
+}
+
+
