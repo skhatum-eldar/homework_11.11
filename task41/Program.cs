cbbf8cfae9620b2a24892ab50484dd68dc83a0ed
Eldar Skhatum <79996321123@ya.ru>
2022-11-29 23:22:35 +0300

    added homework and tasks 41 and 43

---
+++ task41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+Console.Write("Enter the length of the array ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Enter {num} numbers and after each put ENTER");
+int[] array = FillArray(num);
+Console.WriteLine("Your numbers are " + string.Join(", ", array)); 
+
+for (int i = 0, count = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+    if (i == array.Length - 1)
+    {
+        Console.WriteLine("The amount of positive numbers is " + count);
+    }
+}
---
+++ task43/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Enter k1 number ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b1 number ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter k2 number ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter b2 number ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if ((k1 == k2 & b1 == b2) || (k1 == k2))
+{
+    Console.WriteLine("OOPS! TRY AGAIN!");
+}
+else
+{
+    Console.WriteLine("THE COORDINATES OF INTERSECTION OF TWO LINES ARE " + 
+    "(" + (b2-b1)/(k1-k2) + "; " + ((k1*(b2-b1)/(k1-k2)) + b1) + ")");
+}
