d080f6d020e75c9243c6b05d756b107e7f265e6b
Eldar Skhatum <79996321123@ya.ru>
2022-11-30 21:30:18 +0300

    added bonus task with recursion

---
+++ task41.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дополнительная задача на рекурсию (необязательная)
+// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+int count = 0;
+void DigitsAmount (int number)
+{
+    if (number == 0)
+    {
+        return;
+    }
+    count++;
+    DigitsAmount(number/10);
+}
+
+Console.WriteLine("Enter the number");
+int number = Convert.ToInt32(Console.ReadLine());
+DigitsAmount(number);
+Console.Write("The amount of digits is " + count);
