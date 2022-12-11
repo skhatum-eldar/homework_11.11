dfd2e2d920432db5db8511633656e549747879ff
Eldar Skhatum <79996321123@ya.ru>
2022-12-12 00:38:11 +0300

    added homework 64,66,68

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void Numbers (int n)
+{
+    if (n == 0) return;
+    Numbers(n - 1);
+    Console.Write(n + " ");
+}
+
+Console.Write("Enter  the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Numbers(n);
---
+++ task66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int Numbers (int m, int n)
+{
+    if (m >= n)
+    {
+    if (m == n) return m;
+    return n + Numbers(m, n+1);
+    }
+    else
+    {
+    if (m == n) return n;
+    return m + Numbers(m+1, n);
+    }
+}
+
+
+Console.Write("Enter  the number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter  the number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Numbers(m, n));
